ecee87b06422abb69511081b85a444d03eb3ff5b
mahesh-patil20 <104311917+mahesh-patil20@users.noreply.github.com>
2023-06-30 01:25:57 +0530

    static path changed

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
 //static files
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.use(express.static(path.join(__dirname, './frontend/build')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'))
 })
 // const URI = process.env.MONGO_URI;
 
