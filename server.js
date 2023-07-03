d7e6dc4d94abe7e82f926c5b1a5203242feb3d86
mahesh-patil20 <104311917+mahesh-patil20@users.noreply.github.com>
2023-07-03 11:04:48 +0530

    the static path added in server.js which i forgot in the last commit for hosting

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
-//static files
-// app.use(express.static(path.join(__dirname, '../frontend/build')))
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-// })
-// const URI = process.env.MONGO_URI;
+// static files
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+})
+const URI = process.env.MONGO_URI;
 
 app.use('/auth', require('./routes/userRoutes.js'));
 app.use('/recipe', require('./routes/recipeRoutes.js'));
